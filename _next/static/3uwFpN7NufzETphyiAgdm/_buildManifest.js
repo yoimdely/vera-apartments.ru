429bab370e2c75b769630d74fb655e08c7066db0
yoimdely <yoimdely@gmail.com>
2026-02-23 01:50:57 +0300

    Deploy to GitHub Pages

---
+++ _next/static/3uwFpN7NufzETphyiAgdm/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/vera-apartments.ru//_next/:path+",
+        "destination": "/vera-apartments.ru/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- components/ui/separator.tsx
+++
@@ -1,5 +0,0 @@
-export function Separator() {
-  return (
-    <div className="w-full h-px bg-gradient-to-r from-transparent via-brand-gold/20 to-transparent my-0"></div>
-  );
-}
